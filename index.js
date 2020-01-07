a6db83f492e995701628a26218c992deb01196b8
Zakhar Bykov <zak.bykov@gmail.com>
2020-01-07 21:53:19 +0200

    Create a system for connection modules of presentations, documents, spreadsheets.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 
-const Document     = require('./modules/document');
+const Document = require('./modules/document');
 const Presentation = require('./modules/presentation');
-const Spreadsheet  = require('./modules/spreadsheet');
+const Spreadsheet = require('./modules/spreadsheet');
 
 
 class MicrosoftOffice {
@@ -10,12 +11,25 @@ class MicrosoftOffice {
         // code...
     }
 
-    // code...
-    open(file, callback) {
-        // code...
-        var object = {};
 
-        callback(null, object);
+    open(file, callback) {
+        file = path.parse(file);
+
+        if ( fs.existsSync(file.dir + '/' + file.base) ) {
+            if (file.ext === '.doc' || file.ext === '.docx') {
+                callback(null, new Document(file.dir + file.base));
+            } else if (file.ext === '.ppt' || file.ext === '.pptx') {
+                callback(null, new Presentation(file.dir + file.base));
+            } else if (file.ext === '.xls' || file.ext === '.xlsx') {
+                callback(null, new Spreadsheet(file.dir + file.base));
+            } else {
+                callback(new Error("Unknown extension: '" + file.ext + "'. Supported extensions: ppt, pptx, doc, docx [, xls, xlsx not yet]"));
+            }
+        } else if (fs.existsSync(file.dir)) {
+            callback(new Error("File '" + file.base + "' does not exists"));
+        } else {
+            callback(new Error("Directory '" + file.dir + "' does not exists"));
+        }
     }
 }
 
