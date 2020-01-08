e1257ee44b3a0f3de58bb905a7a5c4351b6fd05b
Zakhar Bykov <zak.bykov@gmail.com>
2020-01-08 19:12:47 +0200

    Add elements to Slide.text like: value, mistake, style [...].

--- modules/class/presentation/slide.js
+++ modules/class/presentation/slide.js
@@ -8,10 +8,18 @@ class Slide {
         var textElements = presentation['ppt/slides/slide' + (slideId + 1) + '.xml']['p:sld']['p:cSld'][0]['p:spTree'][0]['p:sp'][0]['p:txBody'][0]['a:p'][0]['a:r'];
 
         textElements.forEach(element => {
-            this.text.push(element['a:t']);
+            this.text.push({
+                value: element['a:t'],
+                mistake: (element['a:rPr'][0]['$']['err']) ? true : false,
+                style: {
+                    bold: (element['a:rPr'][0]['$']['b']) ? true : false,
+                    italic: (element['a:rPr'][0]['$']['i']) ? true : false,
+                    underline: (element['a:rPr'][0]['$']['u']) ? true : false,
+                    strike: (element['a:rPr'][0]['$']['strike']) ? true : false,
+                    fill: (element['a:rPr'][0]['a:solidFill']) ? '#' + element['a:rPr'][0]['a:solidFill'][0]['a:srgbClr'][0]['$']['val'] : '#000000'
+                }
+            });
         });
-
-        // console.log(json['ppt/slides/slide' + (slideId + 1) + '.xml']);
     }
 
     getText() {
